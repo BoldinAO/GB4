6e2af9087336da3cb085bd57aabfc45add061387
Anatoliy <boldinao@icloud.com>
2018-12-22 21:10:42 +0500

    Сделаны задания 3, 4

---
+++ GBHW/HW3/Program.cs
@@ -0,0 +1,71 @@
+﻿//Болдин
+/*
+3. Решить задачу с логинами из предыдущего урока, только логины и пароли считать из файла в массив.
+Создайте структуру Account, содержащую Login и Password.
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account t = new Account();
+            Console.Write(t.LogIn("1", "3"));
+            Console.ReadKey();
+        }
+    }
+
+    class Account
+    {
+        string pathLogin = @"C:\\Users\\Energy\\Desktop\\repo\\GBHW\\HW3\\login.txt";
+        string pathPassword = @"C:\\Users\\Energy\\Desktop\\repo\\GBHW\\HW3\\password.txt";
+
+        private List<string> login = new List<string>() { };
+        private List<string> password = new List<string>() { };
+
+        public string LogIn(string _login, string _password)
+        {
+            SetArr(pathLogin, login);
+            SetArr(pathPassword, password);
+            foreach (string l in login)
+            {
+                if (_login != l)
+                    continue;
+                else
+                {
+                    foreach (string p in password)
+                    {
+                        if (_password == p)
+                        {
+                            return "Вы вошли";
+                        }
+                    }
+                }
+            }
+            return "Не верный логин или пароль";
+        }
+
+        void SetArr(string path, List<string> arr)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        arr.Add(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ GBHW/HW4/Program.cs
@@ -0,0 +1,102 @@
+﻿//Болдин
+/*
+4. *а) Реализовать класс для работы с двумерным массивом. Реализовать конструктор, заполняющий массив случайными числами.
+Создать методы, которые возвращают сумму всех элементов массива, сумму всех элементов массива больше заданного, свойство,
+возвращающее минимальный элемент массива, свойство, возвращающее максимальный элемент массива, метод, возвращающий номер максимального
+элемента массива (через параметры, используя модификатор ref или out)
+*б) Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.
+Дополнительные задачи
+в) Обработать возможные исключительные ситуации при работе с файлами.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxI = 0, maxJ = 0;
+            test t = new test();
+            //Console.WriteLine("Сумма чисел = {0}", t.Max());
+            t.MaxIndex(ref maxI, ref maxJ);
+            Console.WriteLine("Максимальный индекс = {0}.{1}",maxI, maxJ);
+            Console.ReadKey();
+        }
+    }
+
+    class test
+    {
+        static int[,] f = new int[2,2];
+        static Random random = new Random();
+
+        int summ { get; set; }
+        int min { get; set; }
+        int max { get; set; }
+
+        public test()
+        {
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 2; j++)
+                {
+                    f[i, j] = random.Next(0, 100);
+                    Console.Write(f[i, j] + "\t");
+                }
+        }
+
+        public int Summ()
+        {
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine(f[i, j]);
+                    summ += f[i, j];
+                }
+            return summ;
+        }
+
+        public int Min()
+        {
+            min = f[0, 0];
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 2; j++)
+                {
+                    min = min != 0 ? (min > f[i, j] ? f[i, j] : min) : min;
+                }
+            return min;
+        }
+
+        public int Max()
+        {
+            max = f[0, 0];
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine(f[i, j]);
+                    max = max != 0 ? (max < f[i, j] ? f[i, j] : max) : max;
+                }
+            return max;
+        }
+
+        public void MaxIndex(ref int maxI, ref int maxJ)
+        {
+            max = f[0, 0];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    if(max < f[i, j])
+                    {
+                        max = f[i, j];
+                        maxI = i;
+                        maxJ = j;
+                    }
+                }
+            }
+        }
+    }
+}
